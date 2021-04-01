b8643f6d9c70a2bf999a285316244a23828a19b1
速冻鱼 <976499226@qq.com>
2021-04-01 17:12:32 +0800

    完成了项目的基本结构搭建

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <router-view/>
     <h1>黑马头条</h1>
+
+   <i class="toutiao toutiao-shouye"></i>
+   <van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+<div class="box"></div>
   </div>
 </template>
 <script>
@@ -10,4 +18,9 @@ export default {
 }
 </script>
 <style lang="less">
+.box{
+  width: 750px;
+  height: 360px;
+  background-color: blue;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 // 引入全局的样式文件
 import './style/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 使用 lib-flexible 动态设置 REM 基准值（html 标签的字体大小）
+import 'amfe-flexible'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
